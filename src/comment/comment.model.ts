03ec4b892939b54907d627c0c8f46735c15f4367
Sergey-SM2021 <sergey2003.k.96@gmail.com>
2023-08-21 21:50:51 +0300

    fix: create comment

--- src/comment/comment.model.ts
+++ src/comment/comment.model.ts
@@ -5,7 +5,6 @@ import {
   Entity,
   JoinColumn,
   ManyToOne,
-  OneToOne,
   PrimaryGeneratedColumn,
 } from 'typeorm';
 
@@ -13,7 +12,7 @@ import {
 export class Comment {
   @ManyToOne(() => Article, (article) => article.comments)
   article: Article;
-  @OneToOne(() => User)
+  @ManyToOne(() => User)
   @JoinColumn()
   user: User;
   @PrimaryGeneratedColumn()
